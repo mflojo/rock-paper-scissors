8ce86b2d7407db53b7e6c7e116bb1833eafbc046
mflojo <m.eric.flojo@gmail.com>
2025-01-24 09:36:47 -0800

    Add function for user input

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("testing");
+/*  user vs cpu
+    use Math.random() to generate 1-3
+*/
+
+//get user input for choice
+function getHumanChoice() {
+    let input =  prompt("Enter rock, paper or scissors below: ");
+    return input;
+}
+
+console.log(getHumanChoice());      //checking input
