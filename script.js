02157fbf55a2cf67dd2cc28bf640c16267196b00
mflojo <m.eric.flojo@gmail.com>
2025-01-24 11:05:36 -0800

    Add getComputerChoice function

--- script.js
+++ script.js
@@ -2,6 +2,19 @@
     use Math.random() to generate 1-3
 */
 
+//get computer choice
+function getComputerChoice() {
+    let choice = Math.floor(Math.random()*3 + 1);
+    if (choice === 1) {
+        return "rock";
+    } else if (choice === 2) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+console.log(getComputerChoice());
+
 //get user input for choice
 function getHumanChoice() {
     let input =  prompt("Enter rock, paper or scissors below: ");
